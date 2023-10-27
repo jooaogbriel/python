5e0caf6bb2588638927a3ce0f2bb1205bec2c36a
João Gonçalves <joao_i0807@outlook.com>
2023-10-26 23:18:35 -0300

    feat introduction a POO

--- main.py
+++ main.py
@@ -1,66 +1,15 @@
-#comentário
+from package.packing_unpacking import *
 
-"""
-comentário 
-"""
+def packing_unpacking_print():
+    req = {
+        "username": "Joao",
+        "password": "1234",
+        "sum_list": [1,2,3,4,5,6,7,8,9,10]
+    }
+    func_args(*req["sum_list"])
 
-def minha_funcao():
-    return 'Hello Word'
+def main():
+    packing_unpacking_print()
 
-print(type(minha_funcao))
-print(minha_funcao)
-
-my_integer = 10
-print(type(my_integer)) #int
-
-my_string = 'minha string'
-
-my_boolean = True
-
-x = 5
-
-if x > 5:
-    print('x é maior 5')
-elif x == 5:
-    print('x é igual a 5')
-else:
-    print('x é menor que 5') 
-
-x = 6
-y = 10
-
-if x == 5 and y == 10:
-    print('x = 6 e y = 10')
-
-if x == 5 or y == 10:
-    print('OU')
-
-if not x == 20:
-    print('x nao é 20')
-    
-my_string = 'x-salada'
-
-if 'x' in my_string:
-    print('x encontrado')
-
-if 'z' not in my_string:
-    print('z nao encontrado')
-
-my_string = 'Olá M5!'
-
-for char in my_string:
-    print(char)
-
-for index, char in enumerate(my_string):
-    print(index, char)
-
-for number in range(10):
-    print(number)
-
-for number in range(0,10,2):
-    print(number)
-
-
-
-
-
+if __name__ == "__main__":
+    main()
---
+++ package/packing_unpacking.py
@@ -0,0 +1,5 @@
+def func_args(*args):
+    result = 0 
+    for x in args:
+        result += x
+    return print(result)
